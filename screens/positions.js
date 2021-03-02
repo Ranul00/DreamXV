00a04931d0e3893ee07ca1ad5a0a09a9b5dd241b
ashane perera <ashenperera51@yahoo.com>
2021-03-02 09:40:50 +0530

    updtae Position screen

--- screens/positions.js
+++ screens/positions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, TouchableOpacity } from "react-native";
 
 export default function ReviewDetails({ navigation }) {
   const pressHandler = () => {
@@ -8,8 +8,13 @@ export default function ReviewDetails({ navigation }) {
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={pressHandler} style={styles.bckBtnContainer}>
+        <Text style={styles.bckBtnText}>Home</Text>
+      </TouchableOpacity>
+      <View style={styles.topicContainer}>
+        <Text style={styles.topicText}>Positions</Text>
+      </View>
       <Text style={styles.topic}>Select your positions</Text>
-      <Button title="go to home" onPress={pressHandler} />
     </View>
   );
 }
@@ -23,7 +28,29 @@ const styles = StyleSheet.create({
     fontFamily: "FiraSans regular",
     fontSize: 23,
     top: 150,
-    left: 6,
+    left: 8,
+    color: "white",
+  },
+  bckBtnText: {
+    fontSize: 18,
+    color: "white",
+    //fontWeight: "bold",
+    left: 8,
+    top: 42,
+  },
+  topicContainer: {
+    backgroundColor: "#1A1E49",
+    width: 100,
+    height: 38,
+    top: 90,
+    left: 8,
+    borderRadius: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    elevation: 15,
+  },
+  topicText: {
+    alignSelf: "center",
     color: "white",
   },
 });
