0b695aa7bbec5ff5096292bcbe291b3dc0071cc0
Chamikara Kariyapperuma <chamikarakariyapperuma@gmail.com>
2021-04-19 10:08:01 +0530

    [DPM] adding players to list function

--- DataProcessModel.py
+++ DataProcessModel.py
@@ -6,3 +6,22 @@ ws = pd.read_excel("Players.xlsx", "Player Details", index_col=None, na_values=[
 def jsonOutput(worksheet):
     players = []
     rows = worksheet.shape[0]
+
+# players add to list function
+def addToList(worksheet, rows, players):
+    for row in range(0, rows):
+        player = {
+            "name": worksheet.iloc[row, 0],
+            "score": int(worksheet.iloc[row, 1]),
+            "link": worksheet.iloc[row, 2],
+            "img": worksheet.iloc[row, 3],
+            "position": worksheet.iloc[row, 4],
+            "positionNo": int(worksheet.iloc[row, 5]),
+            "height": int(worksheet.iloc[row, 6]),
+            "weight": int(worksheet.iloc[row, 7]),
+            "day": int(worksheet.iloc[row, 8]),
+            "month": int(worksheet.iloc[row, 9]),
+            "year": int(worksheet.iloc[row, 10]),
+            "team": worksheet.iloc[row, 11]
+        }
+        players.append(player)
