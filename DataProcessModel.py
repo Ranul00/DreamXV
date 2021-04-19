0de6346e6641be6ee01624d01e3b54060c3b72b3
Chamikara Kariyapperuma <chamikarakariyapperuma@gmail.com>
2021-04-19 10:06:00 +0530

    [DPM] python dictionary to json object function

--- DataProcessModel.py
+++ DataProcessModel.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
 ws = pd.read_excel("Players.xlsx", "Player Details", index_col=None, na_values=["NA"])
+
+# python dictionary to json object function
+def jsonOutput(worksheet):
+    players = []
+    rows = worksheet.shape[0]
