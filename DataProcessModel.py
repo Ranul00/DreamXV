c928aec907a371b47d9cf0ffbbdeb048f68e510b
Chamikara Kariyapperuma <chamikarakariyapperuma@gmail.com>
2021-04-22 09:05:56 +0530

    created pd_addToList function

--- DataProcessModel.py
+++ DataProcessModel.py
@@ -34,3 +34,22 @@ def pBestTeam():
         bestPlayerList.append(positionSort(ws, x, "Predicted Position No")[0])
 
     return bestPlayerList
+
+def pd_addToList(worksheet, rows, players):
+    for row in range(0, rows):
+        player = {
+            "name": worksheet.iloc[row, 0],
+            "score": int(worksheet.iloc[row, 1]),
+            "link": worksheet.iloc[row, 2],
+            "img": worksheet.iloc[row, 3],
+            "position": worksheet.iloc[row, 4],
+            "positionNo": int(worksheet.iloc[row, 5]),
+            "height": float(worksheet.iloc[row, 6]),
+            "weight": int(worksheet.iloc[row, 7]),
+            "day": int(worksheet.iloc[row, 8]),
+            "month": int(worksheet.iloc[row, 9]),
+            "year": int(worksheet.iloc[row, 10]),
+            "team": worksheet.iloc[row, 11],
+            "predicted pos": worksheet.iloc[row, 31],
+        }
+        players.append(player)
