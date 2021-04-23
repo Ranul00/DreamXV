60033b7534bb6f0c81c112fed5a738d361ad38bd
Ruchira Dias <71806230+ruchiradias001@users.noreply.github.com>
2021-04-23 07:33:42 +0530

    [Pycharm] - DataProcessModel deleted and Refactor to DataProcess also added FlaskAPI  (Ashens Version)

---
+++ Backend - pyCharm/DataProcess.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+
+ws = pd.read_excel("Players.xlsx", "Player Details", index_col=None, na_values=["NA"])
+
+
--- Backend - pyCharm/FlaskAPI.py
+++ Backend - pyCharm/FlaskAPI.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-import DataProcessModel
+import DataProcess
 import pandas as pd
 import json
 
@@ -12,10 +12,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 
-@app.route('/bestTeam')
+@app.route('/bestTeam', methods =["GET"])
 def getBestTeam():
-    team = DataProcessModel.bestTeam()
+    team = DataProcess.bestTeam()
     return json.dumps(team, indent=2)
+
+@app.route('/predictedBestTeam', methods =["GET"])
+def getPredictedBestTeam():
+    team = DataProcess.predictedBestTeam()
+    return json.dumps(team, indent=2)
+
+@app.route('/positionPlayers', methods =["GET"])
+def getPositionPlayers():
+    position = request.args.get('position')
+    players = DataProcess.positionSort(position)
+    return json.dumps(players, indent=2)
+
+@app.route('/search', methods =["GET"])
+def getSearchedPlayers():
+    name = request.args.get('name')
+    players = DataProcess.searchPlayer(name)
+    return json.dumps(players, indent=2)
 
 if __name__ == "__main__":
     app.run(debug=True)
